856cd2f2df944a09e816de003bac3726a7e6e1bd
4201104127 <yamieternal17031997@gmail.com>
2018-12-01 11:59:39 +0700

    first commit

---
+++ WebApplication1/Controllers/testController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[Controller]")]
+    public class testController: Controller
+    {
+        [HttpGet("/api/values")]
+        public IEnumerable<string> Get()
+        {
+            
+            return new string[] { "a", "b" };
+
+        }
+    }
+}
